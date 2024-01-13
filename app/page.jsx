b5663ddfc8f2ebef3af01ae43175c6310ee69630
melkorAMP <melkor@aleph.engineering>
2024-01-13 00:30:17 -0500

    #1 First App template
    - Create a template for the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,22 @@
-
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="w-full flex flex-center flex-col">
+      <h1 className="head_text text-center">
+        Share Your Spark
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          To Light the AI Fire
+        </span>
+      </h1>
+      <p className="desc text-center">
+        DevSparkShare is a collaborative platform for developers to share
+        outstanding prompts, solve real-world issues, and earn tokens for
+        impactful contributions. Built with Next.js and powered by web3
+        technology.
+      </p>
+      
+    </section>
+  );
+};
 
-export default Home
+export default Home;
